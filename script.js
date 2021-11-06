8bf6403f713c1218d8a05a1caff252cd47f17ad0
lkotlarenko <lkotlarenko@protonmail.com>
2021-11-06 17:36:51 -0300

    Creating a function to generate spans with custom classes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World :D');
+const result = document.getElementById('carta-gerada');
+const inputLetter = document.getElementById('carta-texto');
+const button = document.getElementById('criar-carta');
+const style = ['small', 'medium', 'big', 'red', 'green', 'black', 'pink'];
+
+function wordGen(word) {
+  const nWord = document.createElement('span');
+  nWord.innerText = word;
+  nWord.className = style[Math.floor(Math.random() * style.length)];
+  result.appendChild(nWord);
+}
+
+createLetter();
+
+button.addEventListener('click', createLetter);
